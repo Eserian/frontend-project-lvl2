b9c91f01cc519d351ad31f278140594138c25641
Eserian <eserian@mail.ru>
2020-08-09 21:41:56 +0300

    add yml parser

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,23 @@
 import path from 'path';
 import fs from 'fs';
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeAll } from '@jest/globals';
 import genDiff from '../src/index';
 import parser from '../src/parser';
+
+const formats = ['json', 'yml'];
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
-  const before = parser('json', readFile('before.json'));
-  const after = parser('json', readFile('after.json'));
-  const expected = readFile('result.txt');
+let expected;
+
+beforeAll(() => {
+  expected = readFile('result.txt');
+});
+
+test.each(formats)('%s', (format) => {
+  const before = parser(format, readFile(`before.${format}`));
+  const after = parser(format, readFile(`after.${format}`));
 
   expect(genDiff(before, after)).toBe(expected);
 });
--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,8 @@
+import yaml from 'js-yaml';
+
 const mapping = {
   json: JSON.parse,
+  yml: yaml.safeLoad,
 };
 
 export default (type, data) => mapping[type](data);
