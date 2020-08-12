2259614ef629d83f5e79356a0e391675094c1500
Eserian <eserian@mail.ru>
2020-08-12 22:54:20 +0300

    fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -11,6 +11,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(gendiff(filepath1, filepath2, program.format)));
+  .action((filepath1, filepath2) => console.log(`\n${gendiff(filepath1, filepath2, program.format)}`));
 
 program.parse(process.argv);
