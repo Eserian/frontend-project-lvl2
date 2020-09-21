4f7f920765d0d8040ed9ceb5bd58c138c47183d4
eserian <eserian@mail.ru>
2020-09-21 18:58:41 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,27 +1,27 @@
 import _ from 'lodash';
 
-const indent = (dept) => ' '.repeat(2 * dept);
+const indent = (depth) => '  '.repeat(depth);
 
-const stringify = (item, dept) => {
+const stringify = (item, depth) => {
   if (!_.isObject(item)) {
     return item;
   }
-  const result = Object.entries(item).map(([key, value]) => `${indent(dept + 2)}${key}: ${stringify(value, dept + 2)}`);
-  return `{\n${result.join('\n')}\n${indent(dept)}}`;
+  const result = Object.entries(item).map(([key, value]) => `${indent(depth + 2)}${key}: ${stringify(value, depth + 2)}`);
+  return `{\n${result.join('\n')}\n${indent(depth)}}`;
 };
 
 const typeMapping = {
-  added: ({ key, value }, dept) => `${indent(dept)}+ ${key}: ${stringify(value, dept + 1)}`,
-  removed: ({ key, value }, dept) => `${indent(dept)}- ${key}: ${stringify(value, dept + 1)}`,
-  changed: ({ key, value: { oldValue, newValue } }, dept) => `${indent(dept)}- ${key}: ${stringify(oldValue, dept + 1)}\n${indent(dept)}+ ${key}: ${stringify(newValue, dept + 1)}`,
-  unchanged: ({ key, value }, dept) => `${indent(dept)}  ${key}: ${stringify(value, dept + 1)}`,
-  nested: ({ key, children }, dept, f) => `${indent(dept)}  ${key}: ${f(children, dept + 2)}`,
+  added: ({ key, value }, depth) => `${indent(depth)}+ ${key}: ${stringify(value, depth + 1)}`,
+  removed: ({ key, value }, depth) => `${indent(depth)}- ${key}: ${stringify(value, depth + 1)}`,
+  changed: ({ key, value: { oldValue, newValue } }, depth) => `${indent(depth)}- ${key}: ${stringify(oldValue, depth + 1)}\n${indent(depth)}+ ${key}: ${stringify(newValue, depth + 1)}`,
+  unchanged: ({ key, value }, depth) => `${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`,
+  nested: ({ key, children }, depth, f) => `${indent(depth)}  ${key}: ${f(children, depth + 2)}`,
 };
 
-const render = (diff, dept = 1) => {
-  const rendered = diff.map((node) => typeMapping[node.type](node, dept, render));
+const render = (diff, depth = 1) => {
+  const rendered = diff.map((node) => typeMapping[node.type](node, depth, render));
 
-  return `{\n${rendered.join('\n')}\n${indent(dept - 1)}}`;
+  return `{\n${rendered.join('\n')}\n${indent(depth - 1)}}`;
 };
 
 export default render;
